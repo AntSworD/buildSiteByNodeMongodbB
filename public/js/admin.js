3d24e611ca96c27cdedd64f8ca2c0fc53776b460
AntSworD <zhengjj.asd@gmail.com>
2014-12-03 05:38:56 +0000

    jsonp同步豆瓣数据

--- public/js/admin.js
+++ public/js/admin.js
@@ -16,4 +16,30 @@ $(function() {
       }
     });
   });
+
+  $('#douban').blur(function() {
+    var douban = $(this);
+    var id = douban.val();
+
+    if (id) {
+      $.ajax({
+        url: 'https://api.douban.com/v2/movie/subject/' + id,
+        cache: true,
+        type: 'get',
+        dataType: 'jsonp',
+        crossDomain: true,
+        jsonp: 'callback',
+        success: function(data) {
+          $('#inputTitle').val(data.title);
+          $('#inputDoctor').val(data.directors[0].name);
+          $('#inputCountry').val(data.countries[0]);
+          $('#inputLanguage').val(data.languages);
+          $('#inputPoster').val(data.images.large);
+          $('#inputFlash').val(data.trailer_urls);
+          $('#inputYear').val(data.year);
+          $('#inputSummary').val(data.summary);
+        }
+      });
+    }
+  });
 });
