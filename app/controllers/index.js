75d7ae3776c1d565556fbc189df80b0c0a2b24a1
AntSworD <zhengjj.asd@gmail.com>
2014-12-06 16:55:12 +0000

    增加搜索功能，共用列表及分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -21,33 +21,56 @@ exports.index = function(req, res) {
 // search page
 exports.search = function(req, res) {
   var catId = req.query.cat;
-  var page = Number(req.query.p);
+  var page = parseInt(req.query.p, 10) || 0;
   var count = 2;
   var index = page * count;
-  Category
-    .find({_id: catId})
-    .populate({
-      path: 'movies',
-      select: 'title poster',
-      // options: {
-      //   limit: 2,
-      //   skip: index
-      // }
-    })
-    .exec(function(err, categories) {
-      if (err) {
-        console.log(err);
-      }
-      var category = categories[0] || {};
-      var movies = category.movies || [];
-      var results = movies.slice(index, index + count);
-      res.render('results', {
-        title: 'imooc 结果列表页面',
-        keyword: category.name,
-        query: 'cat=' + catId,
-        currentPage: page + 1,
-        totalPage: Math.ceil(movies.length / count),
-        movies: results
+  var q = req.query.q;
+  if (catId) {
+    Category
+      .find({_id: catId})
+      .populate({
+        path: 'movies',
+        select: 'title poster',
+        // options: {
+        //   limit: 2,
+        //   skip: index
+        // }
+      })
+      .exec(function(err, categories) {
+        if (err) {
+          console.log(err);
+        }
+        var category = categories[0] || {};
+        var movies = category.movies || [];
+        var results = movies.slice(index, index + count);
+        res.render('results', {
+          title: 'imooc 结果列表页面',
+          keyword: category.name,
+          query: 'cat=' + catId,
+          currentPage: page + 1,
+          totalPage: Math.ceil(movies.length / count),
+          movies: results
+        });
       });
-    });
+  } else {
+    Movie
+      .find({title: new RegExp(q + '.*', 'i')})
+      .exec(function(err, movies) {
+        if (err) {
+          console.log(err);
+        }
+
+        var results = movies.slice(index, index + count);
+
+        res.render('results', {
+          title: 'imooc 结果列表页面',
+          keyword: q,
+          currentPage: (page + 1),
+          query: 'q=' + q,
+          totalPage: Math.ceil(movies.length / count),
+          movies: results
+        });
+      });
+  }
+
 };
