88b28cd253bb8a53678fab3d22e1e21c5890e819
AntSworD <zhengjj.asd@gmail.com>
2014-12-07 16:15:31 +0000

    安装测试框架并配置gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -36,6 +36,13 @@ module.exports = function(grunt) {
       }
     },
 
+    mochaTest: {
+      options: {
+        reporter: 'spec'
+      },
+      src: ['test/**/*.js']
+    },
+
     concurrent: {
       task: ['nodemon', 'watch'],
       options: {
@@ -47,7 +54,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-nodemon');
   grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-mocha-test');
 
   grunt.option('force', true);
   grunt.registerTask('default', ['concurrent']);
+
+  grunt.registerTask('test', ['mochaTest']);
 };
